f17d189d66ef20f4fde9a2679e0cccd0ad2da889
Nestor Mamani <nmamanipantoja@gmail.com>
2021-10-26 22:02:15 -0500

    Header Styles

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,22 @@
 <template>
-  <header class="flex p-4">
-    <h1>Draw-IDE</h1>
-    <input placeholder="Give your project an amazing name" />
-    <img />
+  <header class="flex p-4 items-center w-full border-b-2">
+    <h1 class="text-3xl font-bold ml-10">Draw-IDE</h1>
+    <input
+      class="
+        bg-gray-200
+        appearance-none
+        border-2 border-gray-200
+        rounded
+        py-2
+        px-4
+        ml-auto
+        w-4/12
+        text-gray-700
+        leading-tight
+        focus:outline-none focus:bg-white focus:border-purple-500
+      "
+      placeholder="Give your project an amazing name"
+    />
+    <img src="/github.svg" width="70" height="70" class="ml-auto" />
   </header>
 </template>
---
+++ components/Whiteboard.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <div id="drawflow"></div>
+  </main>
+</template>
